77d655f9721256e91cf6498c3b4bd2b3743cbe5b
sachcchidanand Gupta <sachchidanandgupta2006@gmail.com>
2025-10-04 08:18:19 +0530

    done with login and signup page

--- netflix-clone-postcss/src/App.jsx
+++ netflix-clone-postcss/src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 export default function App() {
   return (
     <>
@@ -9,6 +12,9 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path ="/login" element={<Login/>}></Route>
+          <Route path ="/signup" element={<Signup/>}></Route>
+          <Route path ="/account" element={<Account/>}></Route>
         </Routes>
       </AuthContextProvider>
     </>
--- netflix-clone-postcss/src/components/Navbar.jsx
+++ netflix-clone-postcss/src/components/Navbar.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 const Navbar = () => {
+  const {user, logOut} = UserAuth()
+  // console.log(user.email)
+  const Navigate = useNavigate();
+  const handleLogout = async () => {
+      try {
+          await logOut();
+           Navigate('/');
+      } catch(error){
+          console.log(error);
+      }
+  }
   return (
-    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-       <h1 className='text-red-600 font-bold text-4xl cursor-pointer'>NETFLIX</h1>
-       <div>
-       <button className='text-white pr-4 '>sign in</button>
-       <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white '>sign up</button>
-       </div>
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <Link to="/">
+        <h1 className="text-red-600 font-bold text-4xl cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+       {user?.email ?  <div>
+        <Link to="/account">
+          <button className="text-white pr-4 ">Account</button>
+        </Link>
+          <button onClick={handleLogout} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white ">
+            LogOut
+          </button>
+      </div> :  <div>
+        <Link to="/login">
+          <button className="text-white pr-4 ">Sign in</button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white ">
+            Sign up
+          </button>
+        </Link>
+      </div>}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
